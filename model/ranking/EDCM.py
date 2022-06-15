c9fd5afbb28b8cf0c4b3a6955a8a60bc9f402951
simon <simontotsuki@gmail.com>
2022-06-15 21:51:03 +0800

    EDCM: runable

--- model/ranking/EDCM.py
+++ model/ranking/EDCM.py
@@ -123,47 +123,46 @@ class EDCM(SocialRecommender,GraphRecommender):
     def trainModel(self):
         self.positive_i = tf.placeholder(tf.int32, shape=[None])
         self.segment_u = tf.placeholder(tf.int32)
+        self.isOn = tf.placeholder(tf.int32)
         self.weights = {}
         self.d_weights = {}
+        self.r_weights = {}
         self.channel_G = {}
-        #self.all_user_embeddings_C = {}
         initializer = tf.contrib.layers.xavier_initializer()
         all_user_embeddings = [tf.identity(self.user_embeddings)]
         all_item_embeddings = [tf.identity(self.item_embeddings)]
-        
-        #Consistency of known friends
+
         for i in range(self.n_layer):
             for j in range(self.n_channel):
-               # self.channel_G['channel_g%d_%d'%(i,j)] = tf.identity(self.S)
-                #self.all_user_embeddings_C['all_user_embeddings%d_%d'%(i,j)] = tf.identity(self.user_embeddings)
                 self.weights['w1%d_%d'%(i,j)] = tf.Variable(initializer([2 * self.emb_size, 1]), name='w1%d_%d'%(i,j))
                 self.weights['w2%d_%d'%(i,j)] = tf.Variable(initializer([self.num_users, 1]), name='w2%d_%d'%(i,j))
                 self.weights['feat%d_%d'%(i,j)] = tf.Variable(initializer([self.emb_size, self.emb_size]), name='feat%d_%d'%(i,j))
             self.weights['agg%d'%i] = tf.Variable(initializer([self.n_channel * self.emb_size, self.emb_size]), name='agg%d'%i)
+        self.r_weights['recommend1'] = tf.Variable(initializer([self.emb_size, self.emb_size]), name='recommend1')
+        #self.r_weights['recommend2'] = tf.Variable(initializer([self.emb_size, self.emb_size]), name='recommend2')
         self.d_weights['discriminator1'] = tf.Variable(initializer([2 * self.emb_size, 2 * self.emb_size]), name='discriminator1')
         self.d_weights['discriminator2'] = tf.Variable(initializer([2 * self.emb_size, self.n_channel]), name='discriminator2')
         
         def loop_user(users):
             u_embedding = tf.reshape(users, [1, self.emb_size]) 
             e_embedding = tf.concat([tf.tile(u_embedding, [self.num_users, 1]), self.user_embeddings], axis=1) #m*2d
-            possibility = tf.nn.relu(tf.matmul(e_embedding, self.weights['w1%d_%d'%(i,j)])) #m*1
+            possibility = tf.matmul(e_embedding, self.weights['w1%d_%d'%(i,j)])
+            possibility = tf.nn.relu(possibility) #m*1 ---------shoule be leaky relu-----------------
             possibility = tf.multiply(possibility, self.weights['w2%d_%d'%(i,j)]) #m*1
             return tf.reduce_sum(possibility, axis=1) #m
-            
+        
         user_set = self.user_embeddings[self.segment_u:self.segment_u+100]
         eps = 1e-10
         loss_edge = 0
         loss_homo = 0
         loss_hetero = 0
         loss_channel = 0
-        for i in range(self.n_layer):
+        for i in range(self.n_layer):  
             for j in range(self.n_channel):
                 raw_possibility = tf.vectorized_map(fn=lambda em:loop_user(em), elems=user_set) #m+*m
                 channel_possibility = tf.nn.softmax(tf.nn.sigmoid(raw_possibility))
-                #self.channel_G['channel_g%d_%d'%(i,j)] = tf.concat([self.channel_G['channel_g%d_%d'%(i,j)][:self.segment_u], channel_possibility, self.channel_G['channel_g%d_%d'%(i,j)][self.segment_u+100:]], axis=0)
                 new_user_embeddings_c = tf.matmul(all_user_embeddings[i], self.weights['feat%d_%d'%(i,j)]) #m*d
-                new_user_embeddings_c = tf.matmul(channel_possibility, new_user_embeddings_c)
-                #self.all_user_embeddings_C['all_user_embeddings%d_%d'%(i,j)] = tf.concat([self.all_user_embeddings_C['all_user_embeddings%d_%d'%(i,j)][:self.segment_u], new_user_embeddings_c, self.all_user_embeddings_C['all_user_embeddings%d_%d'%(i,j)][self.segment_u+100:]], axis=0)
+                new_user_embeddings_c = tf.matmul(channel_possibility, all_user_embeddings[i])
                 semantic_embeddings = tf.concat([all_user_embeddings[i][self.segment_u:self.segment_u+100], new_user_embeddings_c], axis=1)
                 predicted = tf.nn.sigmoid(tf.matmul(semantic_embeddings, self.d_weights['discriminator1']))
                 predicted = tf.nn.softmax(tf.matmul(predicted, self.d_weights['discriminator2']))
@@ -181,13 +180,15 @@ class EDCM(SocialRecommender,GraphRecommender):
                         all_homo_possibility += raw_possibility
                     else:
                         all_hetero_possibility += raw_possibility
-                    
+            
             new_user_embeddings = tf.nn.sigmoid(tf.matmul(new_user_embeddings, self.weights['agg%d'%i]))
-            simple_user_embeddings = tf.sparse_tensor_dense_matmul(self.Y, all_item_embeddings[i])[self.segment_u:self.segment_u+100]
-            new_user_embeddings = (new_user_embeddings + simple_user_embeddings) / 2 
+            new_user_embeddings = tf.nn.l2_normalize(new_user_embeddings, axis=1)
+            
             all_user_embeddings.append(tf.concat([all_user_embeddings[i][:self.segment_u], new_user_embeddings, all_user_embeddings[i][self.segment_u+100:]], axis=0))
-            all_item_embeddings.append(tf.sparse_tensor_dense_matmul(tf.sparse.transpose(self.Y), all_user_embeddings[i]))
+            all_item_embeddings.append(self.item_embeddings)
             all_channel_possibility = tf.nn.sigmoid(all_raw_possibility)
+            all_homo_possibility = tf.nn.sigmoid(all_homo_possibility)
+            all_hetero_possibility = tf.nn.sigmoid(all_hetero_possibility)
             
             s_one_sub = tf.sparse.to_dense(tf.sparse_slice(self.S_one, [self.segment_u,0], [100, self.num_users]))
             s_zero_sub = tf.sparse.to_dense(tf.sparse_slice(self.S_zero, [self.segment_u,0], [100, self.num_users]))
@@ -200,7 +201,7 @@ class EDCM(SocialRecommender,GraphRecommender):
                                         + tf.multiply(tf.cast(tf.logical_not(y_homo_sub>0), tf.float32), tf.log(1-all_homo_possibility+eps))) / (100 * self.num_users) 
             loss_hetero += -tf.reduce_sum(tf.multiply(tf.cast(y_hetero_sub>0, tf.float32), tf.log(all_hetero_possibility+eps)) 
                                           + tf.multiply(tf.cast(tf.logical_not(y_hetero_sub>0), tf.float32), tf.log(1-all_hetero_possibility+eps))) / (100 * self.num_users) 
-            
+                
         user_embeddings = tf.reduce_sum(all_user_embeddings, axis=0)
         item_embeddings = tf.reduce_sum(all_item_embeddings, axis=0)
         
@@ -216,12 +217,13 @@ class EDCM(SocialRecommender,GraphRecommender):
             
         loss = -tf.reduce_sum(tf.log(tf.sigmoid(y))) + self.regU * (
                     tf.nn.l2_loss(self.u_embedding) + tf.nn.l2_loss(self.v_embedding) +
-                    tf.nn.l2_loss(self.neg_item_embedding)) + loss_edge + 10 * (loss_homo + loss_hetero)
+                    tf.nn.l2_loss(self.neg_item_embedding)) 
+        loss_joint = loss_edge + 10 * (loss_homo + loss_hetero)
         
         opt = tf.train.AdamOptimizer(self.lRate)
-        train = opt.minimize(loss, var_list=[self.weights])
+        train_pre = opt.minimize(loss, var_list=[self.r_weights, self.weights])
+        train = opt.minimize(loss+loss_joint, var_list=[self.r_weights, self.weights])
         train_d = opt.minimize(loss_channel, var_list=[self.weights, self.d_weights])
-        # train_h = opt.minimize(loss_h, var_list=[self.weights['query_h']])
         init = tf.global_variables_initializer()
         self.sess.run(init)
         print('Pretrain for consistan weights')
@@ -230,21 +232,22 @@ class EDCM(SocialRecommender,GraphRecommender):
                 user_idx, i_idx, j_idx = batch
                 u_i = np.random.randint(0, self.num_users)
                 i_i = self.positive_sample(u_i)
-                _, l, self.U, self.V = self.sess.run([train, loss, user_embeddings, item_embeddings],
-                                     feed_dict={self.u_idx: user_idx, self.neg_idx: j_idx, self.v_idx: i_idx, self.segment_u: u_i, self.positive_i: i_i})
+                _, l, self.U, self.V = self.sess.run([train_pre, loss, user_embeddings, item_embeddings],
+                                      feed_dict={self.u_idx: user_idx, self.neg_idx: j_idx, self.v_idx: i_idx, self.segment_u: u_i, self.positive_i: i_i, self.isOn: 0})
                 print('training:', epoch + 1, 'batch', n, 'loss:', l)
-            self.ranking_performance(epoch)
+            self.ranking_performance(self.maxEpoch // 2 + epoch)
 
-        # print('Train for hidden users')
-        # for epoch in range(self.maxEpoch // 2):
-        #     for n, batch in enumerate(self.next_batch_pairwise()):
-        #         user_idx, i_idx, j_idx = batch
-        #         u_i = np.random.randint(0, self.num_users)
-        #         i_i = self.positive_sample(u_i)
-        #         _, l, self.U, self.V = self.sess.run([train_h, loss_h, user_embeddings_h, item_embeddings],
-        #                               feed_dict={self.u_idx: user_idx, self.neg_idx: j_idx, self.v_idx: i_idx, self.segment_u: u_i, self.positive_i: i_i})
-        #         print('training:', self.maxEpoch // 2 + epoch + 1, 'batch', n, 'loss:', l)
-        #     self.ranking_performance(self.maxEpoch // 2 + epoch)
+        print('Train for hidden users')
+        for epoch in range(self.maxEpoch // 2):
+            for n, batch in enumerate(self.next_batch_pairwise()):
+                user_idx, i_idx, j_idx = batch
+                u_i = np.random.randint(0, self.num_users)
+                i_i = self.positive_sample(u_i)
+                _, _, l1, l2, l3, l4, self.U, self.V = self.sess.run([train, train_d, loss, loss_edge, loss_homo+loss_hetero, loss_channel, user_embeddings, item_embeddings],
+                                     feed_dict={self.u_idx: user_idx, self.neg_idx: j_idx, self.v_idx: i_idx, self.segment_u: u_i, self.positive_i: i_i, self.isOn: 1})
+                print('training:', self.maxEpoch // 2 + epoch + 1, 'batch', n, 'loss1:', l1, 'loss2:', l2, 'loss3:', l3, 'loss4:', l4)
+            self.ranking_performance(epoch)
+        
         self.U, self.V = self.bestU, self.bestV
         
     def saveModel(self):
